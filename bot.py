e210bff406913f09703d07e86cc72169559e1603
Herbstblatt <stepan.prokopiev@gmail.com>
2021-03-23 18:10:38 +0300

    handle configuration

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
+
+from config import config
+
+def _prefix_callable(bot, msg):
+    # Will be implemented later
+    return commands.when_mentioned_or(config.default_prefix)(bot, msg)
 
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('!'), **kwargs)
+        super().__init__(command_prefix=_prefix_callable, **kwargs)
+
+        for cog in config.cogs:
+            self.load_extension(cog)
 
     async def on_ready(self):
         print('Logged on as {0} (ID: {0.id})'.format(self.user))
@@ -11,4 +20,4 @@ class Bot(commands.Bot):
 
 bot = Bot()
 
-bot.run("")
+bot.run(config.credentials.token)
--- config.py
+++ config.py
@@ -1,2 +1,37 @@
-token = "place your token here"
-cogs = []
+from pathlib import Path
+
+import yaml
+
+class BotConfig:
+
+    def __init__(self, data):
+        self.data = data
+    
+    def __getattr__(self, name):
+        ret = self.data[name]
+        
+        if type(ret) == dict:
+            ret = BotConfig(ret)
+
+        return ret
+
+    def update(self, new):
+        for name, value in new.items():
+            if type(value) == dict:
+                BotConfig(self.data[name]).update(value)
+            else:
+                self.data[name] = value
+    
+    def __iter__(self):
+        for name, value in self.data.items():
+            if type(value) == dict:
+                yield name, BotConfig(value)
+            else:
+                yield name, value
+
+with open("config-default.yml") as f:
+    config = BotConfig(yaml.safe_load(f))
+
+if Path("config.yml").exists():
+    with open("config.yml") as f:
+        config.update(yaml.safe_load(f))
