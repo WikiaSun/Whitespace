326c024d981d6e79ca3ca565b0407753ff454063
Herbstblatt <stepan.prokopiev@gmail.com>
2021-10-12 22:40:06 +0300

    Add an ability to query settings from database

--- utils/settings.py
+++ utils/settings.py
@@ -16,19 +16,43 @@ class WhiteGuild:
         self._bot = bot
         self._pool = bot.pool
 
+    async def query(self, *args):
+        if not args:
+            to_select = "*"
+        else:
+            fields = asdict(self).keys()
+            for field in args:
+                if field not in fields:
+                    raise ValueError("Invalid parameter passed: " + field)
+
+            to_select = ", ".join(args)
+
+        query = f"SELECT {to_select} FROM wh_guilds WHERE id=$1"
+        async with self._pool.acquire() as conn:
+            result = await conn.fetchrow(query, self.id)
+        
+        for field, value in result.items():
+            setattr(self, field, value)
+        
+        return result
+
     async def update(self, **kwargs):
         if not kwargs:
             raise ValueError("No values passed")
         
         query = "UPDATE wh_guilds SET "
+        fields = asdict(self).keys()
         for idx, field in enumerate(kwargs.keys()):
-            if field not in asdict(self).keys():
+            if field not in fields:
                 raise ValueError("Invalid parameter passed: " + field)
 
-            query += f"{field}=${idx + 1}"
-        query += f" WHERE id=${len(kwargs) + 1}"
+            query += f"{field}=${idx + 1} "
+        query += f"WHERE id=${len(kwargs) + 1}"
 
         async with self._pool.acquire() as conn:
             result = await conn.execute(query, *kwargs.values(), self.id)
+
+        for field, value in kwargs.items():
+            setattr(self, field, value)
 
         return result
