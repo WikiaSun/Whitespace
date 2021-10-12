69c216e35896598ab7f7270287ead21a620b8bc3
Herbstblatt <stepan.prokopiev@gmail.com>
2021-10-12 23:24:43 +0300

    Implement custom prefixes

--- bot.py
+++ bot.py
@@ -6,8 +6,12 @@ import slash
 from utils.context import WhiteContext, WhiteInteractionContext
 
 def _prefix_callable(bot, msg):
-    # Will be implemented later
-    return commands.when_mentioned_or(config.default_prefix)(bot, msg)
+    if not msg.guild:
+        prefix = config.default_prefix
+    else:
+        prefix = bot.prefixes[msg.guild.id]
+
+    return commands.when_mentioned_or(prefix)(bot, msg)
 
 class Bot(slash.SlashBot):
     def __init__(self):
