13a08ac4e101decca07e691ab3559ed973b1f465
Herbstblatt <48344463+Herbstblatt@users.noreply.github.com>
2022-10-24 16:31:15 +0300

    Typehint wiki.py

--- utils/wiki.py
+++ utils/wiki.py
@@ -1,31 +1,35 @@
+from typing import Any, Optional
 from urllib.parse import urlencode
+
+import aiohttp
+
 from utils.errors import WikiNotFound
 
 class Wiki:
-    def __init__(self, url=None, id=None, *, session=None):
+    def __init__(self, url: Optional[str] = None, id: Optional[int] = None, *, session: Optional[aiohttp.ClientSession] = None):
         if (url is None) and (id is None):
             raise ValueError("You must specify at least one of: id, url")
 
         self.id = id
         self._session = session
 
-        if url.endswith("/"):
+        if url is not None and url.endswith("/"):
             url = url[:-1]
         self.url = url
 
     @classmethod
-    def from_dot_notation(cls, name, session=None):
-        name = name.split(".")
-        if len(name) == 1:
-            wiki_url = f"https://{name[0]}.fandom.com"
-        elif len(name) == 2:
-            wiki_url = f"https://{name[1]}.fandom.com/{name[0]}"
+    def from_dot_notation(cls, name: str, session: Optional[aiohttp.ClientSession] = None):
+        name_parts = name.split(".")
+        if len(name_parts) == 1:
+            wiki_url = f"https://{name_parts[0]}.fandom.com"
+        elif len(name_parts) == 2:
+            wiki_url = f"https://{name_parts[1]}.fandom.com/{name_parts[0]}"
         else:
             raise WikiNotFound
         
         return cls(url=wiki_url, session=session)
 
-    def url_to(self, page, **params):
+    def url_to(self, page: str, **params) -> str:
         """Returns URL to the given page"""
 
         if not self.url:
@@ -39,7 +43,7 @@ class Wiki:
 
         return url
 
-    def diff_url(self, revid, oldid=None):
+    def diff_url(self, revid: int, oldid: Optional[int] = None) -> str:
         """Returns URL to the given diff"""
         params = {
             "diff": revid
@@ -49,11 +53,13 @@ class Wiki:
         
         return f"{self.url}/?{urlencode(params)}"
     
-    async def query(self, **params):
+    async def query(self, **params) -> dict[str, Any]:
         """Queries MediaWiki api with given params"""
 
         if not self.url:
             raise RuntimeError("Wiki url is required to do this")
+        if self._session is None:
+            raise RuntimeError("This object does not have a session attached to it")
         
         for key, value in params.items():
             if isinstance(value, bool):
@@ -64,12 +70,14 @@ class Wiki:
         async with self._session.get(self.url + "/api.php", params=params) as resp:
             return await resp.json()
     
-    async def query_nirvana(self, **params):
+    async def query_nirvana(self, **params) -> dict[str, Any]:
         """Queries Nirvana with given params"""
 
         if not self.url:
             raise RuntimeError("Wiki url is required to do this")
-
+        if self._session is None:
+            raise RuntimeError("This object does not have a session attached to it")
+        
         params["format"] = "json"
         async with self._session.get(self.url + "/wikia.php", params=params) as resp:
             return await resp.json()
