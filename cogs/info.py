c16d8d13dddf9696a4fba75045bb54757ece19c5
Herbstblatt <48344463+Herbstblatt@users.noreply.github.com>
2022-11-04 23:51:46 +0300

    Merge info and fandom cogs

--- cogs/info.py
+++ cogs/info.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 import random
 from typing import TYPE_CHECKING
+
+import aiohttp
 import discord
 from discord import ui, app_commands
 from discord.ext import commands
@@ -7,6 +10,8 @@ from discord.utils import format_dt
 
 from config import config
 from utils.checks import check_has_flag
+from utils.wiki import Wiki, WikiNotFound
+from utils import converters
 
 if TYPE_CHECKING:
     from ..bot import Bot
@@ -21,7 +26,30 @@ class Info(commands.Cog, name="Информация"):
             return True
         
         return await check_has_flag(ctx, "beta_info_commands_enabled")
+
+    @commands.hybrid_command(name="random")
+    @app_commands.describe(
+        num_start="Нижняя граница диапазона",
+        num_end="Верхняя граница диапазона"
+    )
+    async def random(self, ctx: "WhiteContext", num_start: int, num_end: int):
+        """Выводит случайное число в диапазоне, указанном в параметрах."""
+        await ctx.send(str(random.randint(num_start, num_end)))
     
+
+    @commands.hybrid_command(name="choice")
+    @app_commands.describe(
+        elements="Список элементов для выбора, разделитель — `|`."
+    )
+    async def random_choice(self, ctx, elements: str):
+        """Выбирает случайный элемент из списка."""
+        elements_list = [e.strip() for e in elements.split("|")]
+        if len(elements_list) < 2:
+            await ctx.send(f"{config.emojis.error} | Укажите как минимум два варианта.")
+            return
+
+        await ctx.send(random.choice(elements_list))
+
     @commands.hybrid_command(name="info")
     async def info(self, ctx: "WhiteContext"):
         """Показывает информацию о боте"""
@@ -49,30 +77,76 @@ class Info(commands.Cog, name="Информация"):
         bot_info_view.add_item(ui.Button(label="Пригласить бота", url="https://discord.com/api/oauth2/authorize?client_id=509427930233831444&permissions=939871331&scope=bot%20applications.commands"))
         bot_info_view.add_item(ui.Button(label="Сервер поддержки", url="https://discord.gg/GVvAmTh"))
         await ctx.send(embed=em, view=bot_info_view)
-
-    @commands.hybrid_command(name="random")
-    @app_commands.describe(
-        num_start="Нижняя граница диапазона",
-        num_end="Верхняя граница диапазона"
-    )
-    async def random(self, ctx: "WhiteContext", num_start: int, num_end: int):
-        """Выводит случайное число в диапазоне, указанном в параметрах."""
-        await ctx.send(str(random.randint(num_start, num_end)))
     
+    @commands.hybrid_command()
+    async def page(
+        self,
+        ctx: "WhiteContext",
+        wiki: Wiki = commands.param(converter=converters.WikiConverter),
+        *,
+        name: str = commands.param(converter=converters.PageConverter)
+    ):
+        """Показывает информацию о странице на вики (если не указана, то проверяется вики по умолчанию).
+        
+        Аргументы:
+            wiki: Вики, на которой нужно искать страницу
+            name: Имя страницы
+        """
+        await ctx.defer()
+        if wiki is None:
+            if ctx.settings is None:
+                wiki = Wiki.from_dot_notation("ru.community", session=ctx.bot.session)
+            else:
+                await ctx.settings.query_wiki_info()
+                wiki = ctx.wiki
+        
+        try:
+            data = await wiki.query_nirvana(
+                controller="ArticlesApiController",
+                method="getDetails",
+                titles=name.replace(" ", "_"),
+                abstract=500
+            )
+        except aiohttp.ContentTypeError as exc:
+            raise WikiNotFound from exc
 
-    @commands.hybrid_command(name="choice")
-    @app_commands.describe(
-        elements="Список элементов для выбора, разделитель — `|`."
-    )
-    async def random_choice(self, ctx, elements: str):
-        """Выбирает случайный элемент из списка."""
-        elements_list = [e.strip() for e in elements.split("|")]
-        if len(elements_list) < 2:
-            await ctx.send(f"{config.emojis.error} | Укажите как минимум два варианта.")
+        try:
+            data = list(data["items"].values())[0]
+        except IndexError:
+            await ctx.send(":warning: Данной страницы не существует. Выполняю поиск...")
             return
+        
+        em = discord.Embed(
+            title=data["title"],
+            url=wiki.url_to(name), 
+            description=data["abstract"], 
+            color=0xe35ff5
+        )
+        edit_date = format_dt(datetime.fromtimestamp(int(data['revision']['timestamp'])))
+        em.add_field(
+            name="Последняя правка", 
+            value="{edit_date} от [{user}]({profile}) ([разн.]({diff}) | [история]({history}))".format(
+                edit_date=edit_date,
+                user=data['revision']['user'],
+                profile=wiki.url_to("User:" + data['revision']['user']),
+                diff=wiki.diff_url(data['revision']['id']),
+                history=wiki.url_to(name, action="history")
+            )
+        )
+        if data["thumbnail"]:
+            em.set_thumbnail(url=data["thumbnail"])
+        em.set_footer(text=f"ID: {data['id']}")
+        await ctx.send(embed=em)
 
-        await ctx.send(random.choice(elements_list))
-
+    @page.error
+    async def page_error(self, ctx, error: commands.CommandError):
+        unwrapped = error
+        while original := getattr(unwrapped, "original", None):
+            unwrapped = original
+        
+        if isinstance(unwrapped, WikiNotFound):
+            await ctx.send(f"{config.emojis.error} | Вики с данным адресом не найдена.")
+    
     @commands.hybrid_command(name="serverinfo")
     @app_commands.guild_only()
     async def guildinfo(self, ctx: "WhiteContext"):
--- db/schema.sql
+++ db/schema.sql
@@ -23,6 +23,19 @@ CREATE TABLE public.schema_migrations (
 
 
 --
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    fandom_name text,
+    discord_id bigint,
+    guild_id bigint,
+    trusted boolean,
+    active boolean
+);
+
+
+--
 -- Name: wh_guilds; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -70,6 +83,13 @@ ALTER TABLE ONLY public.wh_wikilink_webhooks
 
 
 --
+-- Name: users_fandom_name_discord_id_guild_id_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX users_fandom_name_discord_id_guild_id_idx ON public.users USING btree (fandom_name, discord_id, guild_id);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
@@ -83,4 +103,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20211006194734'),
     ('20211222113737'),
     ('20211226164418'),
+    ('20220730054441'),
     ('20221023182737');
